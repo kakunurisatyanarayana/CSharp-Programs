a0e52d41bbaeb9a1bd6f15ae5e181e6d104cd868
Satya Kakunuri <47021081+kakunurisatyanarayana@users.noreply.github.com>
2019-02-19 18:48:54 +0530

    Prime Numbers from 1 to Given Number
    Prime Number

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static Boolean isPrime=true;
+    public static Boolean prime(int n)
+    {
+
+        int i, j;
+        int count = 0;
+        for (i = 1; i <= n; i++)
+        {
+            if (n % i == 0)
+                count++;
+        }
+        if (count == 2)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int j;
+        int sum = 0;
+        Console.WriteLine("Enter n");
+        int n = int.Parse(Console.ReadLine());
+        for ( j = 1; j <= n; j++)
+        {
+            isPrime = prime(j);
+            if (isPrime)
+            {
+                sum = sum + j;
+                Console.Write(" "+j);
+            }
+
+        }
+        Console.WriteLine("\n");
+        Console.WriteLine("Sum={0}",sum);
+
+        Console.ReadKey();
+    }
+}
+
